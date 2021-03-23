43ef35c5de67d09900819a1c27a7ea927634bc64
Edwin Vincent <67964674+Edu28Legend@users.noreply.github.com>
2021-03-23 20:09:39 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, flash,render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
+import re
 
 import MySQLdb.cursors
 
@@ -9,7 +10,7 @@ app.secret_key = 'TIGER'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'alappatt'
+app.config['MYSQL_PASSWORD'] = 'vincent98'
 app.config['MYSQL_DB'] = 'quiz'
 
 mysql = MySQL(app)
@@ -17,10 +18,6 @@ mysql = MySQL(app)
 @app.route("/")
 def home():
     return render_template('index.html')
-
-@app.route("/loginpage")
-def loginpage():
-    return render_template('signin.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -35,11 +32,36 @@ def login():
             session['loggedin'] = True
             session['id'] = acc['id']
             session['username'] = acc['username']
-            return redirect(url_for('home'))
+            return redirect(url_for('userhome'))
         else:
             msg = 'Incorrect username or password!'
-    return render_template('signin.html', value = acc , value1=username , msg=msg)
+    return render_template('signin.html', msg=msg)
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    msg = 'Sign up!'
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM account1 WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            cursor.execute('INSERT INTO account1 VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form!'
+    return render_template('register.html', msg=msg)
 
 @app.route("/userhome")
 def userhome():
--- quiz.sql
+++ quiz.sql
@@ -6,18 +6,8 @@ CREATE TABLE IF NOT EXISTS `account1` (
   	`username` varchar(50) NOT NULL,
   	`password` varchar(255) NOT NULL,
   	`email` varchar(100) NOT NULL,
-    `phone` varchar(100) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-INSERT INTO `account1` (`id`, `username`, `password`, `email`,`phone`) VALUES (1, 'Edwin Vincent', 'edu', 'edwinvincent.evt@gmail.com','9876543210');
+INSERT INTO `account1` (`id`, `username`, `password`, `email`) VALUES (1, 'Edwin Vincent', 'edu', 'edwinvincent.evt@gmail.com');
 select * from account1;
-
-CREATE TABLE IF NOT EXISTS `account2` (
-  	`username` varchar(50) NOT NULL,
-  	`password` varchar(255) NOT NULL,
-    PRIMARY KEY (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
-INSERT INTO `account2` (`username`, `password`) VALUES ('Edwin Vincent', 'edu');
-select * from account2;
